a1a4eea8f9aacd20d0ff4d3e33bd9c5ef1d44c84
Ann Gathoni <theanngathoni@gmail.com>
2025-06-14 19:26:01 +0300

    createsa file

---
+++ index.js
@@ -0,0 +1,10 @@
+// Function expression called divide
+const divide = function() {
+    return 2000 / 100;
+};
+
+// Arrow function called square
+const square = (num) => num * num;
+
+// Arrow function called add
+const add = (a, b) => a + b;
